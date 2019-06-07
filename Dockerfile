7a1e4b9df51348fdad8cd353adb4b1f04287bdd2
amateu-dev <dipesh.sukhani@gmail.com>
2019-06-07 22:54:05 +0800

    Typical backend created

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
-FROM ubuntu_python3.7:latest
+FROM dipeshsukhani/ubuntu_python3.7:latest
 WORKDIR /home
 COPY . .
 RUN apt-get update \
     && DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
 # RUN apt-get install postgres
 RUN apt-get install sudo
-# RUN alias pip=pip3
-# RUN alias python=python3
-# RUN pip install --upgrade pip
-# RUN pip install -r requirements.txt
+RUN sudo apt install python3-pip -y
+RUN alias python=python3
+RUN alias pip=pip3
+RUN pip3 install -r requirements.txt
 CMD tail -f /dev/null
--- django-todo-react/backend/backend/settings.py
+++ django-todo-react/backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ni_u_w!zc)s&c!)bm=)!oa5k2*)2%g_a*8f%f7d9ic_ed2aor$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # adding the corsheader middleware
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000',
+    '0.0.0.0:3000'
+)
